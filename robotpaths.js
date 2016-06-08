daf5d94adf9a3b2a7b3b6757fc7aa64390996178
Kamron Batman <kamronbatman@users.noreply.github.com>
2016-06-07 23:01:39 -0700

    [Update] Updates robotpaths with an optimization.
    Updates robotpaths with an optimization.

--- robotpaths.js
+++ robotpaths.js
@@ -1,20 +1,20 @@
 function robotPaths(n) {
-	var map = {};
-	var s = 0;
-	function search(idx) {
-		if (idx >= 0 && idx < n*n && !map[idx]) {
-			if (idx == n*n-1) { s++; }
-			else {
-				map[idx] = 1;
-				search(idx%n>0?idx-1:-1); //Recurse left
-				search(idx+n); //Recurse 1 row below
-				search(n-1>idx%n?idx+1:-1); //Recurse right
-				search(idx-n); //Recurse 1 row above
-				map[idx] = 0;
-			}
-		}
-	}
+  var map = {};
+  var s = 0;
+  function search(idx) {
+    if (idx >= 0 && idx < n*n && !map[idx]) {
+      if (idx == n*n-1) { s++; }
+      else {
+        map[idx] = 1;
+        idx%n>0 && search(idx-1); //Recurse left
+        search(idx+n); //Recurse 1 row below
+        n-1>idx%n && search(idx+1); //Recurse right
+        search(idx-n); //Recurse 1 row above
+        map[idx] = 0;
+      }
+    }
+  }
 	
-	search(0);
-	return s;
+  search(0);
+  return s;
 }
